0378a3d6a168b51fcac62f144a855d578b268756
webopenfathers <1290443925@qq.com>
2023-02-27 20:32:25 +0800

    2023 2-27 20:32 typescript运行环境和简单体验

---
+++ 01_TypeScript语法基础/03_HelloTypeScript.js
@@ -0,0 +1,8 @@
+"use strict";
+// exports.__esModule = true;
+// string:TypeScript给我们定义标识符，提供的字符串类型
+// String:JavaScript中字符串的包装类
+var message = "hello world";
+message = "Hello TypeScript";
+// message = true;
+console.log(message);
---
+++ 01_TypeScript语法基础/03_HelloTypeScript.ts
@@ -0,0 +1,8 @@
+// string:TypeScript给我们定义标识符，提供的字符串类型
+// String:JavaScript中字符串的包装类
+let message: string = "hello world";
+message = "Hello TypeScript";
+// message = true;
+console.log(message);
+
+export {};
