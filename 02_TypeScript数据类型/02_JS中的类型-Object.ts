d57281675b57795cb98f723481bd1738dc1540ba
webopenfathers <1290443925@qq.com>
2023-03-01 19:42:49 +0800

    2023 3-1 19:42 typescript的数组类型和对象类型

---
+++ 02_TypeScript数据类型/02_JS中的类型-Object.ts
@@ -0,0 +1,14 @@
+let info: {
+  name: string;
+  age: number;
+  height: number;
+} = {
+  name: "zbw",
+  age: 18,
+  height: 1.88,
+};
+
+console.log(info.name);
+console.log(info.age);
+
+export {};
