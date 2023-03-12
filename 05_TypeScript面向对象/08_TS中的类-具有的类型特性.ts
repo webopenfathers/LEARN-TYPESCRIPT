69af1c999f1c53f569b997b483fbe4733f0a861c
webopenfathers <1290443925@qq.com>
2023-03-12 21:03:01 +0800

    2023  3-12 21:02 TS面向对象类-具有的类型特性

---
+++ 05_TypeScript面向对象/08_TS中的类-具有的类型特性.ts
@@ -0,0 +1,16 @@
+class Person {}
+
+/**
+ * 类的作用：
+ * 1.可以创建类对应的实例对象
+ * 2.类本身可以作为这个实例的类型
+ * 3.类也可以当作有一个构造签名的函数
+ */
+const name: string = 'aaa';
+const p: Person = new Person();
+function printPerson(p: Person) {}
+
+function factory(ctor: new () => void) {}
+factory(Person);
+
+export {};
