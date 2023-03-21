6eb5e95e0cbf102652ca63775817fc153c1ee341
webopenfathers <1290443925@qq.com>
2023-03-21 21:44:55 +0800

    2023  3-21 21:44 TS扩展-第三方包的类型声明处理方案

--- 08_TypeScript知识扩展-webpack/src/index.ts
+++ 08_TypeScript知识扩展-webpack/src/index.ts
@@ -1,7 +1,27 @@
 import { sum } from './utils/math';
+import axios from 'axios';
+import type { AxiosRequestConfig, AxiosInstance } from 'axios'
+import React from 'react'
+import _ from 'loadsh'
+
 
 const message: string = 'Hello Word';
 
 console.log(message.length, message);
 
 console.log(sum(20, 30));
+
+
+// lib.dom.d.ts
+const h2El = document.createElement('h2')
+h2El.textContent = 'Hello TypeScript'
+document.body.append(h2El)
+
+// lib.es2015.d.ts
+const promise = new Promise((resolve, reject) => { })
+
+// axios
+
+// loadsh
+console.log(_.join(['abc', 'cba']));
+
---
+++ 08_TypeScript知识扩展-webpack/types/zbw.d.ts
@@ -0,0 +1,5 @@
+// 声明loadsh模块
+declare module 'loadsh' {
+  // 里面是各种方法
+  export function join(...arg: any[]): any
+}
